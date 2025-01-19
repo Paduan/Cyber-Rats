d06102dd8e72651d409fb202a512c94fa8ac7d90
paduanjj <rafaelpaduan1@gmail.com>
2025-01-19 20:11:40 -0300

    modified:   cyber-rats/src/Desafio2.css 	modified:   cyber-rats/src/Desafio2.jsx

--- cyber-rats/src/Desafio2.jsx
+++ cyber-rats/src/Desafio2.jsx
@@ -7,7 +7,7 @@ function Desafio2() {
     <div className="container">
       <h2>Desafio 2</h2>
       <p>Bem-vindo ao Desafio 2! Resolva os problemas e teste suas habilidades.</p>
-      <span className="hidden-hacker">@H4cker-from-sky-99</span> {}
+      <div data-hidden-text="@H4cker-from-sky-99"></div> {/* Texto escondido no atributo */}
     </div>
   );
 }
